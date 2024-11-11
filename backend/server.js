c9e910d957186a2e45b31bfd783341e3db5343a5
jamesrashid226 <jamesrashid226@gmail.com>
2024-11-10 18:47:04 -1000

    added cor

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,17 @@ const productRoute = require('./routes/api/productRoute');
 let mongodb_url = 'mongodb://localhost/';
 let dbName = 'yolomy';
 
-const MONGODB_URI = 'mongodb://mongodb-0.mongodb:27017/yolomy';  // mongo-db is the container name// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://james_rashid:james_rashid@cluster0.bwtll.mongodb.net/yolomy?retryWrites=true&w=majority&appName=Cluster0'
-//const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/yolomy';
+const MONGODB_URI = 'mongodb://mongodb-0.mongodb:27017/yolomy'; // MongoDB URI
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 let db = mongoose.connection;
 
 // Check Connection
-db.once('open', ()=>{
+db.once('open', () => {
     console.log('Database connected successfully')
 })
 
 // Check for DB Errors
-db.on('error', (error)=>{
+db.on('error', (error) => {
     console.log(error);
 })
 
@@ -31,18 +30,18 @@ const app = express()
 // Body parser middleware
 app.use(express.json())
 
-// 
-app.use(upload.array()); 
+// Upload middleware
+app.use(upload.array());
 
-// Cors 
+// CORS configuration to allow any origin
 app.use(cors());
 
 // Use Route
-app.use('/api/products', productRoute)
+app.use('/api/products', productRoute);
 
 // Define the PORT
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
